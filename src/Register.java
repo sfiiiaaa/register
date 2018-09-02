a4d3ff2c0063bc2e6c0d010f6acc124db6134b7e
sfiiiaaa <38935296+sfiiiaaa@users.noreply.github.com>
2018-09-02 15:29:44 +0700

    Create GUI ( First )

---
+++ src/Register.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Register {
+    private JLabel txtRegis;
+    private JLabel txtUser;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JFormattedTextField formattedTextField1;
+    private JButton OKButton;
+    private JButton BACKButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
